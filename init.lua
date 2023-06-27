cac8f57a19c7fe90421e9575ba6e994951c5b658
Dominik Karski <dominik-karski@wp.pl>
2023-06-27 11:05:23 +0200

    New remaps

--- init.lua
+++ init.lua
@@ -32,6 +32,10 @@ vim.g.mapleader = ' ' -- Make sure to set `mapleader` before lazy so your mappin
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
 vim.keymap.set('n', '<leader>Y', [["+Y]])
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 -- Package manager --
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
